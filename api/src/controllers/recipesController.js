6da7d38e82b810607571fcea434020bc94ce1db2
Pedro Daniel Chi Novelo <danihellnovelo18@gmail.com>
2022-05-26 13:58:00 -0500

    new apikey

--- api/src/controllers/recipesController.js
+++ api/src/controllers/recipesController.js
@@ -1,6 +1,6 @@
 const { Recipe, Diet } = require("../db.js");
 const axios = require("axios");
-const { API_KEY, API_KEY2, API_KEY3, API_KEY4, API_KEY5} = process.env;
+const { API_KEY, API_KEY2, API_KEY3, API_KEY4, API_KEY5, API_KEY6} = process.env;
 
 const { Op } = require("sequelize");
 function removeTags(str){
@@ -12,7 +12,7 @@ const getAllrecipes = async (req, res, next) => {
   if (req.query.name) return next();
   try {
     const api = await axios.get(
-      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY5}&addRecipeInformation=true&number=100`
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY6}&addRecipeInformation=true&number=100`
     );
     const db = await Recipe.findAll({
       include: {
@@ -74,7 +74,7 @@ const getRecipeByName = async (req, res, next) => {
   const lowerCaseQueryName = name.toLowerCase();
   try {
     const api = await axios.get(
-      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY5}&addRecipeInformation=true&number=100`
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY6}&addRecipeInformation=true&number=100`
     );
     const db = await Recipe.findAll({
       where: {
@@ -189,7 +189,7 @@ const getRecipeById = async (req, res, next) => {
           );
     } else if (regexExpNum(id)) {
       const api = await axios.get(
-        `https://api.spoonacular.com/recipes/${id}/information?apiKey=${API_KEY5}`
+        `https://api.spoonacular.com/recipes/${id}/information?apiKey=${API_KEY6}`
       );
       const { data } = api;
       let apiResponse = (() => {
